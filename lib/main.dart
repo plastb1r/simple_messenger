736fc06ac3fc34384ad3b714d96ccd737a659cb3
plastb1r <eremin@surfstudio.ru>
2021-06-17 12:33:41 +0300

    moved provider on top

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mwwm/mwwm.dart';
 import 'package:provider/provider.dart';
@@ -8,25 +9,30 @@ import 'interactor/message/repository/message_repository.dart';
 import 'ui/screens/choose_name/pick_name_screen.dart';
 import 'utils/default_error_handler.dart';
 
-void main() => runApp(App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        home: MultiProvider(
-          providers: [
-            Provider(
-              create: (_) => WidgetModelDependencies(
-                errorHandler: DefaultErrorHandler(),
-              ),
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          Provider(
+            create: (_) => WidgetModelDependencies(
+              errorHandler: DefaultErrorHandler(),
             ),
-            Provider(
-              create: (_) => MessageInteractor(
-                repo: MessageRepository(FirebaseFirestore.instance),
-              ),
+          ),
+          Provider(
+            create: (_) => MessageInteractor(
+              repo: MessageRepository(FirebaseFirestore.instance),
             ),
-          ],
-          child: PickNameScreen(),
+          ),
+        ],
+        child: MaterialApp(
+          home: PickNameScreen(),
         ),
       );
 }
