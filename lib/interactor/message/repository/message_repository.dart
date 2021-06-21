479814da97e7f1a39381ad99c7c746e727650116
plastb1r <eremin@surfstudio.ru>
2021-06-21 13:22:54 +0300

    took out a constants + changed dynamic to Object

--- lib/interactor/message/repository/message_repository.dart
+++ lib/interactor/message/repository/message_repository.dart
@@ -2,38 +2,35 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:simple_messenger/data/message.dart';
+import 'package:simple_messenger/utils/constants.dart';
 
 class MessageRepository {
   final FirebaseFirestore _source;
 
-  const MessageRepository(FirebaseFirestore source) : _source = source;
+  const MessageRepository(this._source);
 
   Future<void> sendMessage(Message message) =>
-      _source.collection('messages').add(message.toMap());
+      _source.collection(kMessageCollection).add(message.toMap());
 
   Stream<List<Message>> getMessages() => _source
-      .collection('messages')
-      .orderBy('timestamp')
-      .snapshots()
-      .asyncMap<List<Message>>(_snapshotParser);
+          .collection(kMessageCollection)
+          .orderBy(kTimestampField)
+          .snapshots()
+          .asyncMap<List<Message>>((snapshot) {
+        final objectSnapshot = snapshot.docs
+            .map((doc) => doc.data().cast<String, Object>())
+            .toList();
 
-  FutureOr<List<Message>> _snapshotParser(
-    QuerySnapshot<Map<String, dynamic>> snapshot,
+        return _snapshotParser(objectSnapshot);
+      });
+
+  List<Message> _snapshotParser(
+    List<Map<String, Object>> docs,
   ) {
-    final docs = snapshot.docs;
-
     if (docs.isEmpty) {
       return [];
     }
-    return docs.map(_messageParser).toList();
-  }
 
-  Message _messageParser(
-    QueryDocumentSnapshot<Map<String, dynamic>> doc,
-  ) {
-    final map = doc.data();
-    final message = Message.fromMap(map);
-
-    return message;
+    return docs.map((doc) => Message.fromMap(doc)).toList();
   }
 }
